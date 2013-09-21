d3bf74f7fc60b11f2f74a501ba379df476284c3c
Stuart Ward <stuart.ward@bcs.org>
2013-09-21 19:12:06 +0100

    updsate

--- db/SetProfileTable.py
+++ db/SetProfileTable.py
@@ -3,13 +3,34 @@ from db.models import TankTempParams
 from db.models import Temp
 from db.models import Profile
 
-ParamSets = TankTempParams.objects.all()
 
-for ParamSet in ParamSets do
-   StartTime = ParamSet.start_time
-   EndTime = ParamSet.end_time
-   TempReadings = Temp.objects.get(time__gte=StartTime time__lte=EndTime)
-   
-   for Reading in TempReadings do
-   
+def SetProfileTable(ForDay, ForTank)
+
+   ParamSets = TankTempParams.objects.get(tank=ForTank)
+
+   ParamSet = ParamSets.objets.get(start_time__gte = ForDay, end_time__lte = ForDay)
+   if len(ParamSet) = 1:
+      for TimePeriod in range(0,23):
+         StartTime = ForDay.replace(hour=TimePeriod)
+         EndTime = ForDay.replace(hour=TimePeriod,minute=59)
+
+         TempReadings = Temp.objects.get(time__gte=StartTime time__lte=EndTime)
+         SumTemp=0
+         NumReadings=0
+         for Reading in TempReadings:
+            SumTemp=+ Reading.temp
+            NumReadings=+1
+         AveTemp = SumTemp/NumReadings
+         
+         LuxReadings = Lux.objects.get(time__gte=StartTime time__lte=EndTime)
+         SumLux=0
+         NumReadings=0
+         for Reading in LuxReadings:
+            SumLux=+ Reading.Lux
+            NumReadings=+1
+         AveLux = SumLux/NumReadings
+         
+         ProfileTargetEntry = Profile(tank=ForTank, time=ForDay.replace(hour=TimePeriod), temp=AveTemp lux=AveLux)
+
+
 
