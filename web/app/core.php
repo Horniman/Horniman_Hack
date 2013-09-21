280a2811b12f87f1ff1e6ae9ab925325454e65fa
Darren Mothersele <darren@darrenmothersele.com>
2013-09-21 14:32:59 +0100

    test callback

--- web/app/core.php
+++ web/app/core.php
@@ -10,7 +10,8 @@
 /**
  * Bootstrap the system
  */
-function leak_bootstap() {
+function leak_bootstrap() {
+  chdir(dirname(realpath(__FILE__)));
   // TODO: Parse the request
   // TODO: Connect to the database
   // TODO: Load the current user
@@ -24,5 +25,38 @@ function leak_execute() {
   // TODO: Check permissions by defined access callback
   // TODO: Execute the request via defined callback
   // TODO: Format the response
+
+  // Temporary, just to check everything is working, this will be removed:
+  require_once 'test/test.inc';
+  $vars = test_request();
+  $vars['page_title'] = 'LEAK';
+
+  // Format output depending on requested format
+  // (hardcode to html initially)
+  $format = 'html';
+  switch ($format) {
+    case 'html':
+      echo leak_template('html', $vars);
+      break;
+  }
 }
+
+
+/**
+ * Convert some variables to rendered output
+ */
+function leak_template($template_name, $variables) {
+  extract($variables, EXTR_SKIP);
+  // Start output buffering
+  ob_start();
+  // Include the template file
+  include 'templates/' . $template_name . '.tpl.php';
+  // End buffering and return its contents
+  return ob_get_clean();
+}
+
+
+
+
+
 
---
+++ web/app/templates/html.tpl.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--[if IE 8]>         <html class="no-js lt-ie9" lang="en" > <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js" lang="en" > <!--<![endif]-->
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title><?php print $page_title; ?></title>
+  <link rel="stylesheet" href="css/foundation.css">
+  <script src="js/vendor/custom.modernizr.js"></script>
+</head>
+<body>
+  <h1><?php print $page_title; ?></h1>
+  <?php print $content; ?>
+</body>
+</html>
