7f347aab5afe75e4055b604e05a9aed9d9bbe369
Darren Mothersele <darren@darrenmothersele.com>
2013-09-21 22:52:25 +0100

    adding observations missing fle

---
+++ web/app/controllers/observations/list.tpl.php
@@ -0,0 +1,5 @@
+<?php foreach ($observations as $observation): ?>
+  <?php // TODO: list observations; ?>
+<?php endforeach; ?>
+<?php print l('Add observation', 'observations/add',
+  array('class' => 'button')); ?>
