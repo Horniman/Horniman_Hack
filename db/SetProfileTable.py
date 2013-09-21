b41f2850b06b62ff8bdad724d86c5d10a469957d
Stuart Ward <stuart.ward@bcs.org>
2013-09-21 20:59:24 +0100

    Update

--- db/SetProfileTable.py
+++ db/SetProfileTable.py
@@ -5,39 +5,44 @@ from db.models import Profile
 from timedate import timedelta
 
 
-def SetProfileTable(ForDay, ForTank)
+def SetProfileTable(ForDay, ForTank):
 
    TempParamSets = TankTempParams.objects.get(tank=ForTank)
 
    TempParamSet = TempParamSets.objets.get(start_time__gte = ForDay, end_time__lte = ForDay)
    if len(TempParamSet) <> 1:
-      
+      return 1 # error number of TemParamaterSets 
    LuxParamSets = TankLuxParams.objects.get(tank=ForTank)
    
    LuxParamSet = LuxParamSets.objets.get(start_time__gte = ForDay, end_time__lte = ForDay)
-      TempTimeOffset=TempParamSet[0].time_offset_seconds
-      for TimePeriod in range(0,23):
-         StartTime = ForDay.replace(hour=TimePeriod) - timedelta(seconds=TempTimeOffset)
-         EndTime = ForDay.replace(hour=TimePeriod,minute=59)
-
-         TempReadings = Temp.objects.get(time__gte=StartTime time__lte=EndTime)
-         SumTemp=0
-         NumReadings=0
-         for Reading in TempReadings:
-            SumTemp=+ Reading.temp
-            NumReadings=+1
-         AveTemp[TimePeriod] = SumTemp/NumReadings
+   if len(TempParamSet) <> 1:
+      return 1 # error number of LuxParamaterSets 
    
-         
-         LuxReadings = Lux.objects.get(time__gte=StartTime time__lte=EndTime)
-         SumLux=0
-         NumReadings=0
-         for Reading in LuxReadings:
-            SumLux=+ Reading.Lux
-            NumReadings=+1
-         AveLux[TimePeriod] = SumLux/NumReadings
-         
-         ProfileTargetEntry = Profile(tank=ForTank, time=ForDay.replace(hour=TimePeriod), temp=AveTemp lux=AveLux)
-         ProfileTargetEntry.save()
+   TempTimeOffset=TempParamSet[0].time_offset_seconds
+   LuxTimeOffset=LuxParamSet[0].time_offset_seconds
+   
+   for TimePeriod in range(0,23):
+      StartTime = ForDay.replace(hour=TimePeriod) - timedelta(seconds=TempTimeOffset)
+      EndTime = ForDay.replace(hour=TimePeriod,minute=59)
+
+      TempReadings = Temp.objects.get(time__gte=StartTime, time__lte=EndTime)
+      SumTemp=0
+      NumReadings=0
+      for Reading in TempReadings:
+         SumTemp=+ Reading.temp
+         NumReadings=+1
+      AveTemp[TimePeriod] = SumTemp/NumReadings
+
+      
+      LuxReadings = Lux.objects.get(time__gte=StartTime, time__lte=EndTime)
+      SumLux=0
+      NumReadings=0
+      for Reading in LuxReadings:
+         SumLux=+ Reading.Lux
+         NumReadings=+1
+      AveLux[TimePeriod] = SumLux/NumReadings
+      
+      ProfileTargetEntry = Profile(tank=ForTank, time=ForDay.replace(hour=TimePeriod), temp=AveTemp, lux=AveLux)
+      ProfileTargetEntry.save()
 
 
